dfd305cad3c8c21b32d1826198980a9748d60a74
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-10-29 16:22:13 +0000

    added file

---
+++ hexadecimal.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Created by: Seti Ngabo
+# Created on: Oct 2021
+# This program translates strings into unicode characters
+
+import constants
+
+
+def hexa_decimal(user_input):
+    my_characters = []
+    numbers = ""
+
+    for numbers in user_input:
+        if numbers in constants.DICTIONARY.keys():
+            my_characters.append(constants.DICTIONARY[numbers])
+        else:
+            continue
+
+    return my_characters
+
+
+def main():
+    # this function uses an associative array
+
+    # input
+    user_input = input("Enter a string to be translated into hex: ")
+
+    # call functions
+    final_answer = hexa_decimal(user_input)
+
+    # output
+    print("\n{0} in hexa is {1}.".format(user_input, final_answer))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
